4a24c48d3d9ba70634cc2636aa3b61cf4c32605b
radu2020 <radu.ask@gmail.com>
2020-05-15 19:34:17 +0300

    Initial commit

---
+++ src/main/java/com/spring/eventsplanner/EventsPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.eventsplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsPlannerApplication.class, args);
+	}
+
+}
