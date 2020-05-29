1fc38b5d709eb0f15b0ed52f466c095fef40cc43
radu2020 <radu.ask@gmail.com>
2020-05-30 01:06:36 +0300

    checkpoint

--- src/main/java/com/spring/eventsplanner/controller/CatalogController.java
+++ src/main/java/com/spring/eventsplanner/controller/CatalogController.java
@@ -1,5 +1,17 @@
 package com.spring.eventsplanner.controller;
 
-public class CatalogController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/catalog")
+public class CatalogController {
+	
+	@GetMapping("/list")
+	public String locationsList(Model theModel) {
+		
+		return null;
+	}
 }
