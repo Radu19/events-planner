10f51a5e9797e86ad2535d4c33c27da21ca7c132
radu2020 <radu.ask@gmail.com>
2020-06-01 08:46:55 +0300

    CHECKPOINT
    implement catalog-list.html

--- src/main/java/com/spring/eventsplanner/controller/CatalogController.java
+++ src/main/java/com/spring/eventsplanner/controller/CatalogController.java
@@ -1,17 +1,29 @@
 package com.spring.eventsplanner.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.spring.eventsplanner.entity.Location;
+import com.spring.eventsplanner.service.LocationService;
+
 @Controller
 @RequestMapping("/catalog")
 public class CatalogController {
 	
+	@Autowired
+	private LocationService locationService;
+	
 	@GetMapping("/list")
 	public String locationsList(Model theModel) {
 		
-		return null;
+		List<Location> locations = locationService.findAll();
+		theModel.addAttribute("locations", locations);
+		
+		return "/catalog/catalog-list";
 	}
 }
